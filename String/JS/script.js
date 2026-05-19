03c25f0a6bff6ddef915dbbf10aa8b1ab6947977
GustavoFrota <gustavofro90@gmail.com>
2026-05-19 15:02:07 -0300

    Adicionando nomeCompleto

--- String/JS/script.js
+++ String/JS/script.js
@@ -1,4 +1,6 @@
 var nome = "Gustavo";
+var sobrenome = "Frota";
+var nomeCompleto = nome + " " + sobrenome
 
-console.log(nome);
-console.log(typeof nome);
+console.log(nomeCompleto);
+console.log(typeof nomeCompleto);
