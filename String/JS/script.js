10784af4e828addb90186942c5e0f8b35c664fa9
GustavoFrota <gustavofro90@gmail.com>
2026-05-19 15:04:54 -0300

    Nenhuma alteração

--- String/JS/script.js
+++ String/JS/script.js
@@ -2,5 +2,9 @@ var nome = "Gustavo";
 var sobrenome = "Frota";
 var nomeCompleto = nome + " " + sobrenome
 
+
+console.log(nome);
+console.log(typeof nome);
+
 console.log(nomeCompleto);
 console.log(typeof nomeCompleto);
