47dae83cccea3f94da60620a535addba031b22d9
GustavoFrota <gustavofro90@gmail.com>
2026-05-20 14:16:48 -0300

    Adicionando melhorias ao if

--- Condicional/script.js
+++ Condicional/script.js
@@ -1,3 +1,9 @@
-if(true) {
-    console.log("Testando o if")
+console.log("Antes do if")
+
+var idade = 18;
+
+if(idade >= 18) {
+    console.log("Pode tirar a carteira de motorista")
 }
+
+console.log("Despois do if")
