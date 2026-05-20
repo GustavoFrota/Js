174859c95a15f63419c508858bfa1b6453f682db
GustavoFrota <gustavofro90@gmail.com>
2026-05-20 14:28:04 -0300

    Adicionando a condicional else

--- Condicional/script.js
+++ Condicional/script.js
@@ -4,6 +4,8 @@ var idade = 18;
 
 if(idade >= 18) {
     console.log("Pode tirar a carteira de motorista")
+} else {
+    console.log("O usuário ainda não pode tirar a carteira")
 }
 
 console.log("Despois do if")
