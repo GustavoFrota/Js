4423d7fb4a80176ce0dfc05030f643eabfd344d6
GustavoFrota <gustavofro90@gmail.com>
2026-05-20 14:46:38 -0300

    Adicionando else if

--- Condicional/script.js
+++ Condicional/script.js
@@ -2,10 +2,13 @@ console.log("Antes do if")
 
 var idade = 18;
 
-if(idade >= 18) {
-    console.log("Pode tirar a carteira de motorista")
+if(idade > 18) {
+    console.log("Pode tirar a carteira de motorista.")
+} else if(idade == 18) {
+    console.log("Você tem 18 anos.")
 } else {
-    console.log("O usuário ainda não pode tirar a carteira")
+    console.log("O usuário ainda não pode tirar a carteira.")
 }
+
 
 console.log("Despois do if")
