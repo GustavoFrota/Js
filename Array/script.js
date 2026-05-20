d51aea6769ec3ff6519c0bb7dcc3c693534dca91
GustavoFrota <gustavofro90@gmail.com>
2026-05-20 09:14:56 -0300

    Inserindo e alterando elementos

--- Array/script.js
+++ Array/script.js
@@ -4,6 +4,10 @@ console.log(arr)
 var numero = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
 console.log(numero);
 
-//Acessando itens
+//Acessando elementos
 console.log(arr[1]);
 console.log(numero[6]);
+
+//Inserindo e alterando elementos
+arr[4] = 10;
+arr[0] = "Teste";
