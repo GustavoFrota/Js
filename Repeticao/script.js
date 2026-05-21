798d051239503eb8be60538c4d151b0e7e983b13
GustavoFrota <gustavofro90@gmail.com>
2026-05-21 15:53:23 -0300

    Adicionando comentário

--- Repeticao/script.js
+++ Repeticao/script.js
@@ -2,7 +2,8 @@ var x = 0;
 
 while(x < 5) {
     console.log("Testando o while " + x);
-    x++;
+
+    x++; // x = x + 1
 }
 
 var arr = ["teste", "testando", "a", "b"];
@@ -11,5 +12,5 @@ var y = 0;
 while(y <=3) {
     console.log(arr[y]);
 
-    y++;
+    y++; // y = y + 1
 }
