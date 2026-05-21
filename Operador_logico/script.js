2c865437b007b55db08d7894ace3e4e479545639
GustavoFrota <gustavofro90@gmail.com>
2026-05-21 14:19:48 -0300

    Adicionando o Operador ||

--- Operador_logico/script.js
+++ Operador_logico/script.js
@@ -1,8 +1,15 @@
-var idade = 16
-var nome = "Gustavo"
+var idade = 16;
+var nome = "Gustavo";
 
 if(nome == "Gustavo" && idade >= 16) {
-    console.log("O Gustavo pode entrar")
+    console.log("O Gustavo pode entrar");
 } else {
-    console.log("Este não é o Gustavo")
+    console.log("Este não é o Gustavo");
+}
+
+var identidade = "Guilherme";
+var age = 16;
+
+if (identidade == "Guilherme" || age >= 18) {
+    console.log("Pode entrar");
 }
