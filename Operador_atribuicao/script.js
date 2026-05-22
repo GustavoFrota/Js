4f89f7db1e8c9386d5ef230a26e4a800aa6c353f
GustavoFrota <gustavofro90@gmail.com>
2026-05-22 14:12:10 -0300

    Outros exemplos

--- Operador_atribuicao/script.js
+++ Operador_atribuicao/script.js
@@ -1,5 +1,15 @@
 var x = 1;
 var y = 2;
 
+//Soma
 console.log(x = x + y); // Resposta 3
 console.log(x += y); // Resposta 5
+
+//Subtração
+console.log(x -= y);
+
+//Multiplicação
+console.log(x *= y);
+
+//Divisão
+console.log(x /= y);
