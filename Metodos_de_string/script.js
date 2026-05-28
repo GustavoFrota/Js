2c4d0c7166da9f405c1f86f75d4a5998465358bf
GustavoFrota <gustavofro90@gmail.com>
2026-05-28 14:13:32 -0300

    Adicionando indexOf

--- Metodos_de_string/script.js
+++ Metodos_de_string/script.js
@@ -1,2 +1,5 @@
 var nome = 'Gustavo';
 console.log(nome.length) //length é uma propriedade usada principalmente para saber o tamanho de algo
+
+const frutas = ['banana', 'uva', 'morango', 'laranja'];
+console.log(frutas.indexOf('uva')); //uva está no indíce 1
