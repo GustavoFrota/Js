e290b0f8197efb99416ef0b4d6cfd96f249228ec
GustavoFrota <gustavofro90@gmail.com>
2026-05-28 14:35:03 -0300

    Adicionando replace

--- Metodos_de_string/script.js
+++ Metodos_de_string/script.js
@@ -7,5 +7,8 @@ console.log(frutas.indexOf('uva')); //uva está no indíce 1
 var frase = "O rato roeu a roupa do rei de Roma";
 console.log(frase.indexOf("Roma"));
 
-var roeu = frase.slice(7, 11); //serve para copiar uma parte de um array ou string sem alterar o original
+var roeu = frase.slice(7, 11); // slice serve para copiar uma parte de um array ou string sem alterar o original
 console.log(roeu);
+
+var novaFrase = frase.replace("roeu", "teste"); // replace serve para substituir partes de uma string por outro valor
+console.log(novaFrase);
