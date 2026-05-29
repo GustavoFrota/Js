1ff32412bacbdf7c6fcb3a61f70b08d661ef9475
GustavoFrota <gustavofro90@gmail.com>
2026-05-29 14:32:27 -0300

    Adicionando pop

--- Metodos_de_array/script.js
+++ Metodos_de_array/script.js
@@ -3,3 +3,7 @@ var arr = [1, 2, 3, 4, 5];
 arr.push(6); // push é um método de array usado para adicionar um ou mais elementos no final de um array
 
 console.log(arr.length); // length é usada para indicar o tamanho de algo
+
+var frutas = ["uva", "abacaxi", "laranja", "banana", "mamão"];
+frutas.pop();  // pop serve para remover o último elemento de um array e retornar esse elemento
+console.log(frutas);
