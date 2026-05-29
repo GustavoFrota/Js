85eb07ee58c896ba09e5a370ac087bbb01a8a1dc
GustavoFrota <gustavofro90@gmail.com>
2026-05-29 14:23:19 -0300

    Adicionando push

--- Metodos_de_array/script.js
+++ Metodos_de_array/script.js
@@ -1,2 +1,5 @@
-var arr = [1, 2, 3, 4, 5]
-console.log(arr.length) // length é usada para indicar o tamanho de algo
+var arr = [1, 2, 3, 4, 5];
+
+arr.push(6); // push é um método de array usado para adicionar um ou mais elementos no final de um array
+
+console.log(arr.length); // length é usada para indicar o tamanho de algo
