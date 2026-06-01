f233e08332a0f0bf520702be4e8d944b0a29a8f2
GustavoFrota <gustavofro90@gmail.com>
2026-06-01 09:21:10 -0300

    Adicionando o  método falar

--- Nossos_Objetos/script.js
+++ Nossos_Objetos/script.js
@@ -1,6 +1,11 @@
 let Pessoa = {
     nome: "Gustavo",
-    idade: 19
+    idade: 19,
+    falar: function() {
+        console.log("Olá, tudo bem ?")
+    }
 }
 
 console.log(Pessoa.nome);
+
+Pessoa.falar();
