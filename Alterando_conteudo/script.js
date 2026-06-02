29cd80f3db676b2d208f8b07131514bdbc5927f7
GustavoFrota <gustavofro90@gmail.com>
2026-06-02 14:32:23 -0300

    Utilizaando textContent

--- Alterando_conteudo/script.js
+++ Alterando_conteudo/script.js
@@ -2,3 +2,9 @@ var title = document.querySelector('#title');
 
 //innerHTML
 title.innerHTML = "Testando o texto alterado";
+
+var subtitle = document.querySelector('#subtitle');
+
+//textContent -> Mais utilizado
+
+subtitle.textContent = "Testando o textContent";
