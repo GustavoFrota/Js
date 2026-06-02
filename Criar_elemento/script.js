c30c07de3cb0bb99aabc6254793020b728bf79ae
GustavoFrota <gustavofro90@gmail.com>
2026-06-02 15:42:52 -0300

    Criando elemento DOM

---
+++ Criar_elemento/script.js
@@ -0,0 +1,7 @@
+var novoParagrafo = document.createElement("p");
+
+console.log(novoParagrafo);
+
+var texto = document.createTextNode("Este é o texto do parágrafo");
+
+novoParagrafo.appendChild(texto);
