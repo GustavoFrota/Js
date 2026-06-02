5a91963fa19f96daa06570c2b60ca8620d812db5
GustavoFrota <gustavofro90@gmail.com>
2026-06-02 09:49:05 -0300

    Utilizando querrySelector

--- Selector/script.js
+++ Selector/script.js
@@ -7,3 +7,9 @@ console.log(itensQuerry);
 var itensQuerry2 = document.querySelectorAll('#lista .item');
 
 console.log(itensQuerry2);
+
+// querrySelector
+
+var itensQuerry3 = document.querySelector('#lista .item');
+
+console.log(itensQuerry3);
