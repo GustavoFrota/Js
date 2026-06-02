951a381eb1cf2ee594d797f64ebb11abd1d89d46
GustavoFrota <gustavofro90@gmail.com>
2026-06-02 15:48:48 -0300

    Inserindo elemento no body

--- Criar_elemento/script.js
+++ Criar_elemento/script.js
@@ -1,3 +1,5 @@
+// Inserido elemento no body
+
 var novoParagrafo = document.createElement("p");
 
 console.log(novoParagrafo);
@@ -5,3 +7,8 @@ console.log(novoParagrafo);
 var texto = document.createTextNode("Este é o texto do parágrafo");
 
 novoParagrafo.appendChild(texto);
+
+var body = document.querySelector('body');
+console.log(body);
+
+body.appendChild(novoParagrafo);
