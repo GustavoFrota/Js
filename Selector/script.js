226b37b14708f4918dd71b34e0aa8bb9094fbd12
GustavoFrota <gustavofro90@gmail.com>
2026-06-02 09:38:56 -0300

    Utilizando querrySelectorAll

--- Selector/script.js
+++ Selector/script.js
@@ -1,3 +1,9 @@
+// querrySelectorAll
+
 var itensQuerry = document.querySelectorAll('#lista2 li');
 
 console.log(itensQuerry);
+
+var itensQuerry2 = document.querySelectorAll('#lista .item');
+
+console.log(itensQuerry2);
