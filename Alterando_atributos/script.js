d30fec908da69f28154558fa5ebe4576d22b4779
GustavoFrota <gustavofro90@gmail.com>
2026-06-03 15:36:59 -0300

    Alterando atributos

---
+++ Alterando_atributos/script.js
@@ -0,0 +1,9 @@
+var title = document.querySelector('#title');
+
+title.setAttribute("class", "testando-atributo"); // serve para definir ou alterar um atributo de um elemento HTML no DOM
+
+console.log(title);
+
+var btn = document.querySelector('#btn');
+
+btn.setAttribute("disabled", "disabled");
