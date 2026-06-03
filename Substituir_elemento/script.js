99f1623fd6f58a4e234d9cf384be04002697baf3
GustavoFrota <gustavofro90@gmail.com>
2026-06-03 14:15:14 -0300

    Alterando elementos

--- Substituir_elemento/script.js
+++ Substituir_elemento/script.js
@@ -7,3 +7,13 @@ var texto = document.createTextNode("Este é o texto do h3")
 elemento.appendChild(texto);
 
 console.log(elemento);
+
+// selecionar o elemento que quero mudar
+
+var title = document.querySelector('#title');
+
+console.log(title);
+
+var body = document.querySelector('body');
+
+body.replaceChild(elemento, title);
