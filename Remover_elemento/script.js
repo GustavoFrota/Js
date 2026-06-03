ed6a670eea9d057090b230bd5daeb908a9ee1c80
GustavoFrota <gustavofro90@gmail.com>
2026-06-03 08:59:09 -0300

    Removendo elemento

--- Remover_elemento/script.js
+++ Remover_elemento/script.js
@@ -1,5 +1,13 @@
+// Removendo elemento filho
+
 var container = document.querySelector('#container');
 
 var p = document.querySelector('#container p');
 
 container.removeChild(p);
+
+// Removendo elemento
+
+var subtitle = document.querySelector('#subtitle');
+
+subtitle.remove();
