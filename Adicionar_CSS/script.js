7f3fbcfce11332fdccc8505a9027f816b1c47ea3
GustavoFrota <gustavofro90@gmail.com>
2026-06-09 08:58:33 -0300

    Adicionando vários estilos

--- Adicionar_CSS/script.js
+++ Adicionar_CSS/script.js
@@ -3,3 +3,7 @@ var title = document.querySelector('#title');
 title.style.color = 'red';
 
 title.style.backgroundColor = 'yellow';
+
+var subtitle = document.querySelector('#subtitle');
+
+subtitle.style.cssText = "color: blue; background-color: pink; font-size: 50px";
