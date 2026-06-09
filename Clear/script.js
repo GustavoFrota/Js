8bcc06da6af4ee97a7a4b6ba7a6e07bee7d848ab
GustavoFrota <gustavofro90@gmail.com>
2026-06-09 16:05:12 -0300

    Utilizando clearInterval

--- Clear/script.js
+++ Clear/script.js
@@ -10,3 +10,12 @@ if(x > 0) {
     clearTimeout(mytime);
     console.log("O x passou de 0")
 }
+
+var myinterval = setInterval(function() {
+    console.log("Imprimindo intervalo")
+}, 500);
+
+setTimeout(function() {
+    console.log("Não precisa mais repetir")
+    clearInterval(myinterval)
+}, 1500);
