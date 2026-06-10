c96748d9ce08ffc670f9c31622ae67c7af88590b
GustavoFrota <gustavofro90@gmail.com>
2026-06-10 10:26:30 -0300

    Adicionando evendo de duplo clique

--- Click/script.js
+++ Click/script.js
@@ -12,3 +12,9 @@ var title = document.querySelector('#title');
 title.addEventListener("click", function() {
     console.log("Título");
 })
+
+var subtitle = document.querySelector('#subtitle');
+
+subtitle.addEventListener("dblclick", function() {
+    console.log("clicou duas vezes");
+})
