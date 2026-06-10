a6bd5b81085e866abeb2f7cf5d2fd4d5553bed22
GustavoFrota <gustavofro90@gmail.com>
2026-06-10 10:23:59 -0300

    Adicionando evento de click no título

--- Click/script.js
+++ Click/script.js
@@ -6,3 +6,9 @@ btn.addEventListener("click", function() {
     console.log("Você clicou");
     alert("você clicou");
 })
+
+var title = document.querySelector('#title');
+
+title.addEventListener("click", function() {
+    console.log("Título");
+})
